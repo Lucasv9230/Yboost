7bc5d0daa48ea0d56b8fcf24a4bfb187d9fe0709
Lucas <lucas.vauclin@ynov.com>
2026-03-25 15:35:47 +0100

    add

---
+++ aaaammjjhhss/helper.js
@@ -0,0 +1,20 @@
+const success =(message, data) =>{
+    return {
+        message: message,
+        data: data
+    }
+}
+
+module.exports = success;
+
+/*
+exports.success = (message, data) => {
+    return {
+        message: message,
+        data: data
+    }
+}
+*/
+exports.success = (message, data) => {
+    return {message, data}
+}
